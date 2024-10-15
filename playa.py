a782bec35c012a5d4f5f34c32b6c66acca945f14
Santiago de Leon <sdeleon28@gmail.com>
2024-10-15 17:02:19 -0300

    improve UI

--- playa.py
+++ playa.py
@@ -2,13 +2,21 @@ import sys
 import click
 import vlc
 import readchar
+import curses
+import threading
+import time
 
 class AudioPlayer:
-    def __init__(self, playlist):
+    def __init__(self, playlist, stdscr):
         self.playlist = playlist
+        self.stdscr = stdscr
         self.player = vlc.MediaPlayer()
         self.current_index = 0
+        self.mode = 'current_song'
+        self.running = True
+        self.show_help = False
         self.play_song(self.current_index)
+        threading.Thread(target=self.update_ui, daemon=True).start()
 
     def play_song(self, index):
         if index < 0 or index >= len(self.playlist):
@@ -48,23 +56,74 @@ class AudioPlayer:
     def play_pause(self):
         self.player.pause()
 
-@click.command()
-@click.argument('files', nargs=-1, type=click.Path(exists=True), required=False)
-@click.option('-f', '--file', 'playlist_file', type=click.File('r'), help='Playlist file')
-def main(files, playlist_file):
-    playlist = []
-    if '-' in files:
-        playlist = [line.strip() for line in sys.stdin if line.strip()]
-    elif playlist_file:
-        playlist = [line.strip() for line in playlist_file if line.strip()]
-    elif files:
-        playlist = list(files)
-    else:
-        click.echo('No input files provided')
-        sys.exit(1)
+    def toggle_mode(self):
+        if self.mode == 'current_song':
+            self.mode = 'playlist'
+        else:
+            self.mode = 'current_song'
 
-    player = AudioPlayer(playlist)
+    def display_help(self):
+        self.show_help = not self.show_help
 
+    def update_ui(self):
+        while self.running:
+            self.stdscr.clear()
+            if self.show_help:
+                self.display_help_screen()
+            elif self.mode == 'current_song':
+                self.display_current_song()
+            else:
+                self.display_playlist()
+            self.stdscr.refresh()
+            time.sleep(0.1)
+
+    def display_current_song(self):
+        title = f"Now Playing: {self.playlist[self.current_index]}"
+        duration = self.player.get_length() // 1000
+        current_time = self.player.get_time() // 1000
+        progress = 0
+        if duration > 0:
+            progress = current_time / duration
+        progress_bar = '[' + ('#' * int(progress * 50)).ljust(50) + ']'
+        time_info = f"{self.format_time(current_time)} / {self.format_time(duration)}"
+        self.stdscr.addstr(1, 2, title)
+        self.stdscr.addstr(3, 2, progress_bar)
+        self.stdscr.addstr(5, 2, time_info)
+
+    def display_playlist(self):
+        self.stdscr.addstr(1, 2, "Playlist:")
+        for idx, song in enumerate(self.playlist):
+            if idx == self.current_index:
+                self.stdscr.addstr(3 + idx, 4, f"> {song}")
+            else:
+                self.stdscr.addstr(3 + idx, 6, song)
+
+    def display_help_screen(self):
+        commands = [
+            "j: Next song",
+            "k: Previous song",
+            "h: Seek backward 10s",
+            "l: Seek forward 10s",
+            "1-0: Jump to song (1-10)",
+            "q-p: Progress bar seek",
+            "Space: Play/Pause",
+            "Esc: Toggle UI mode",
+            "?: Toggle help screen",
+            "Ctrl+C: Exit"
+        ]
+        self.stdscr.addstr(1, 2, "Help:")
+        for idx, cmd in enumerate(commands):
+            self.stdscr.addstr(3 + idx, 4, cmd)
+
+    def format_time(self, seconds):
+        m, s = divmod(int(seconds), 60)
+        return f"{m}:{s:02d}"
+
+    def stop(self):
+        self.running = False
+        self.player.stop()
+
+def main_loop(player):
     while True:
         key = readchar.readchar()
         if key == 'j':
@@ -84,8 +143,37 @@ def main(files, playlist_file):
             player.progress_bar_seek(key)
         elif key == ' ':
             player.play_pause()
+        elif key == '\x1b':
+            player.toggle_mode()
+        elif key == '?':
+            player.display_help()
         elif key == '\x03':
+            player.stop()
             break
+
+@click.command()
+@click.argument('files', nargs=-1, type=click.Path(exists=True), required=False)
+@click.option('-f', '--file', 'playlist_file', type=click.File('r'), help='Playlist file')
+def main(files, playlist_file):
+    playlist = []
+    if '-' in files:
+        playlist = [line.strip() for line in sys.stdin if line.strip()]
+    elif playlist_file:
+        playlist = [line.strip() for line in playlist_file if line.strip()]
+    elif files:
+        playlist = list(files)
+    else:
+        click.echo('No input files provided')
+        sys.exit(1)
+
+    curses.wrapper(run_player, playlist)
+
+def run_player(stdscr, playlist):
+    player = AudioPlayer(playlist, stdscr)
+    try:
+        main_loop(player)
+    except KeyboardInterrupt:
+        player.stop()
 
 if __name__ == '__main__':
     main()
